abf633c3345378ae8fb2d8544a3cfc32b6c401f9
zwebbmack <85358349+zwebbmack@users.noreply.github.com>
2021-10-04 14:42:00 -0400

    Added Example3 and Example4, type and syntax errors in Python

---
+++ CF2-Example3.py
@@ -0,0 +1,14 @@
+'''Example 3: a few lines of code with a type error'''
+
+num1 = 4.0
+num2 = 5.9
+num3 = 2
+
+var = "a"
+
+list = [num1, num2, num3, var]
+
+
+S = sum(list)
+
+print(S)
---
+++ CF2-Example4.py
@@ -0,0 +1,8 @@
+'''Example 4: a few lines of code with a (few) syntax error(s)'''
+import math
+
+list = [1, 2, 3, pi]
+
+max = max(list)
+min = min(list)
+print(float(max + min)
