08bfb475e5789b85be6a40238746bb0ed5884c12
zwebbmack <85358349+zwebbmack@users.noreply.github.com>
2021-10-04 14:33:43 -0400

    Added Example2 file in C++

---
+++ CF2-Example2.cpp
@@ -0,0 +1,12 @@
+/* Professor's instructions: write a hello world program
+   in a language of your choosing */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  /Print "Hello, world!"
+  std::cout << "Hello, world!";
+  return 0
+}}
